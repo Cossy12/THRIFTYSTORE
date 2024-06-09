e45af50301ffc577846a7344b9daa758e88cac51
Cossy <cosmasmutuku001@gmail.com>
2024-06-09 22:09:04 +0300

    create manageuser form and views

--- accounts/forms.py
+++ accounts/forms.py
@@ -43,3 +43,39 @@ class ManagerUserForm(UserCreationForm):
 
 
 
+
+class CustomerUserForm(UserCreationForm):
+	'''
+	Form that uses built-in UserCreationForm to handel user creation
+	'''
+	first_name = forms.CharField(max_length=30, required=True,
+		widget=forms.TextInput(attrs={'placeholder': '*Your first name..'}))
+	last_name = forms.CharField(max_length=30, required=True,
+		widget=forms.TextInput(attrs={'placeholder': '*Your last name..'}))
+	username = forms.EmailField(max_length=254, required=True,
+		widget=forms.TextInput(attrs={'placeholder': '*Email..'}))
+	password1 = forms.CharField(
+		widget=forms.PasswordInput(attrs={'placeholder': '*Password..','class':'password'}))
+	password2 = forms.CharField(
+		widget=forms.PasswordInput(attrs={'placeholder': '*Confirm Password..','class':'password'}))
+	password = forms.CharField(required=False,
+		widget=forms.PasswordInput(attrs={'placeholder': '*Confirm Password..','class':'password'}))
+
+	#reCAPTCHA token
+	token = forms.CharField(
+		widget=forms.HiddenInput())
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'password1', 'password2','password')
+
+	def save(self, commit=True):
+		user = super().save(commit=False)
+		user.is_customer = True
+		if commit:
+			user.save()
+		return user
+
+
+
+
