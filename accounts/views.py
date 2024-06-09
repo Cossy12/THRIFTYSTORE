e3f11c672990da2ea5fa4cd719588a30db5ec0f7
Cossy <cosmasmutuku001@gmail.com>
2024-06-09 22:08:19 +0300

    create manageuser form and views

--- accounts/views.py
+++ accounts/views.py
@@ -26,7 +26,96 @@ def  is_ajax(request):
 
 # Create your views here.
 
+class CustomerSignUpView(AjaxFormMixin, FormView):
+	'''
+	Generic FormView with our mixin for user sign-up with reCAPTURE security
+	'''
 
+	template_name = "registration/customer_signup.html"
+	form_class = CustomerUserForm
+	success_url = "/"
+
+	#reCAPTURE key required in context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["recaptcha_site_key"] = settings.RECAPTCHA_PUBLIC_KEY
+		return context
+
+	#over write the mixin logic to get, check and save reCAPTURE score
+	def form_valid(self, form):
+		response = super(AjaxFormMixin, self).form_valid(form)	
+		if is_ajax(self.request):
+			token = form.cleaned_data.get('token')
+			captcha = reCAPTCHAValidation(token)
+			if captcha["success"]:
+				obj = form.save()
+				obj.email = obj.username
+				obj.save()
+				up = obj.userprofile
+				up.captcha_score = float(captcha["score"])
+				up.save()
+				
+				login(self.request, obj, backend='django.contrib.auth.backends.ModelBackend')
+
+				#change result & message on success
+				result = "Success"
+				# message = "Thank you for signing up"
+				message = 'The app site is under update, please try again later", "sorry for the inconvinience'
+
+				server_d = 'sorry'
+				server_d_ms = 'The app site is under update, please try again later", "sorry for the inconvinience'
+			
+				
+			data = {'result': result, 'message': message,'server_d':server_d,'server_d_ms':server_d_ms}
+			return JsonResponse(data)
+
+		return response
+
+
+class ManagerSignUpView(AjaxFormMixin, FormView):
+	'''
+	Generic FormView with our mixin for user sign-up with reCAPTURE security
+	'''
+
+	template_name = "registration/plugmanager_signup.html"
+	form_class = ManagerUserForm
+	success_url = "/"
+
+	#reCAPTURE key required in context
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context["recaptcha_site_key"] = settings.RECAPTCHA_PUBLIC_KEY
+		return context
+
+	#over write the mixin logic to get, check and save reCAPTURE score
+	def form_valid(self, form):
+		response = super(AjaxFormMixin, self).form_valid(form)	
+		if is_ajax(self.request):
+			token = form.cleaned_data.get('token')
+			captcha = reCAPTCHAValidation(token)
+			if captcha["success"]:
+				obj = form.save()
+				obj.email = obj.username
+				obj.save()
+				up = obj.userprofile
+				up.captcha_score = float(captcha["score"])
+				up.save()
+				
+				login(self.request, obj, backend='django.contrib.auth.backends.ModelBackend')
+
+				#change result & message on success
+				result = "Success"
+				# message = "Thank you for signing up"
+				message = 'The app site is under update, please try again later", "sorry for the inconvinience'
+
+				server_d = 'sorry'
+				server_d_ms = 'The app site is under update, please try again later", "sorry for the inconvinience'
+			
+				
+			data = {'result': result, 'message': message,'server_d':server_d,'server_d_ms':server_d_ms}
+			return JsonResponse(data)
+
+		return response
 
 
 
